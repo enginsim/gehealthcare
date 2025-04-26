770d90b3717d25510a25132f8dc14cb28eb78114
Liam <liam@liamco.io>
2025-04-26 13:53:58 +0100

    add import and call for country ratings function

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from src.tradingeconomics_data_extractor import get_tradingeconomics_data
 from src.oecd_data_extractor import get_oecd_data
 from src.worldbank_data_extractor import get_worldbank_data
 from src.countryeconomy_data_extractor import get_countryeconomy_data
+from src.countryeconomy_data_extractor import get_country_ratings
 
 def main():
     get_oecd_data()
     get_worldbank_data()
     get_countryeconomy_data()
     get_allianz_data()
+    get_country_ratings()
     
     #get_tradingeconomics_data()
 

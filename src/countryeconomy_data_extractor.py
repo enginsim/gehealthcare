0bcb719e5efdf84d4b35b6dd7fac9b1be9225661
Liam <liam@liamco.io>
2025-04-26 15:42:40 +0100

    make country ratings function

--- src/countryeconomy_data_extractor.py
+++ src/countryeconomy_data_extractor.py
@@ -25,10 +25,55 @@ def get_countries():
 
     return countries
 
+
 def get_country_ratings(country_name):
     #e.g. https://countryeconomy.com/ratings/usa
-    url = 'https://countryeconomy.com/ratings/' + country_name.lower()
-    print(f'getting {country_name} ratings')
+    url = 'https://countryeconomy.com/ratings/'
+
+    #get page and html 
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Error fetching the page: {e}")
+        return []
+    
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    #empty list to store countries and their rating data
+    country_ratings_data = {}
+
+    #get table rows
+    rows = soup.select("tbody tr")
+
+    #get cells from rows and links from cells
+    for row in rows:
+        cells = row.find_all("td")
+        link = cells[0].find("a")
+
+        #remove unwanted characters/spaces from country name
+        if link:
+            clean_name = link.text.strip().replace("[+]", "").strip()
+
+            if clean_name == country_name:
+                #get cells for each rating for the chosen country
+                moody_cell = cells[1].find("span", class_="padleft")
+                sp_cell = cells[2].find("span", class_="padleft")
+                fitch_cell = cells[3].find("span", class_="padleft")
+
+                #clean ratings and replace missing data
+                rating_moody = moody_cell.text.strip() if moody_cell else "Unavailable"
+                rating_sp = sp_cell.text.strip() if sp_cell else "Unavailable"
+                rating_fitch = fitch_cell.text.strip() if fitch_cell else "Unavailable"
+
+                print(f"""The ratings for {country_name} are:
+            Moody's: {rating_moody}
+            S&P: {rating_sp}
+            Fitch: {rating_fitch}
+            """)
+    
+    return
+
     
 def get_countryeconomy_data():
     print('getting countryeconomy data...')
